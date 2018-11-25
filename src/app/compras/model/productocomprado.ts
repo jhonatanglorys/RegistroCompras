8cf889c3233cf851857137411d98f6dad19a8b40
jhonatanglorys <jhonatanglorys@gmail.com>
2018-11-24 20:08:30 -0500

    nueva clase de producto comprado

---
+++ src/app/compras/model/productocomprado.ts
@@ -0,0 +1,11 @@
+export class Productocomprado {
+   
+          nombreProd:string;
+          vUnitario:number;
+          cantidadComprada:number;
+          ivaDelProductoComprado:number;
+          ivaDeLosProductosComprados:number;
+          subtotalDeLaCompra:number;
+          totalCompra:number;
+       
+}
